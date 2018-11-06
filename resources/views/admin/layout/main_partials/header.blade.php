b213b86eeb1f4f558ccfc025d79605dc573835c6
ashargilani <ashar.gillani@coeus-solutions.de>
2018-11-07 00:47:12 +0500

    Set the main index and dashboard pagge

---
+++ resources/views/admin/layout/main_partials/header.blade.php
@@ -0,0 +1,15 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="author" content="Ashar Gillani" />
+    <title>@yield('page-title')</title>
+    <link rel="shortcut icon" href="assets/favicon.ico" />
+    <script src="https://use.fontawesome.com/e07b5b2f1c.js"></script>
+    <link href="{{ asset('css/bootstrap.min.css')}}" rel="stylesheet"  >
+    <link href="{{ asset('css/datatables.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/simple-sidebar.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/jquery.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+</head>
